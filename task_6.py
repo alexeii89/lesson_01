6fb5b1eb8b0a6ca98d3edbb9b1a5f19733b92609
ALexeii <mr.white1989@rambler.ru>
2020-12-20 01:32:43 +0500

    EDIT

--- task_6.py
+++ task_6.py
@@ -3,7 +3,7 @@ day = 1
 objective = int(input("Цель пробежать: "))
 print(f"{day}-й день: {result:.2f}")
 while result < objective:
-    result = result + result * 0.1
+    result = result + result*0.1
     day += 1
     print(f"{day}-й день: {result:.2f}")
 print(f"на {day}-й день спортсмен достиг результата — не менее {objective} км.")
