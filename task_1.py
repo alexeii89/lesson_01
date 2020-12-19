29d3a2dc79f37f237fa3861e29a61d178b56acc7
ALexeii <mr.white1989@rambler.ru>
2020-12-19 21:45:43 +0500

    Update task_1.py

--- task_1.py
+++ task_1.py
@@ -2,4 +2,3 @@ your_name = input("Введите Ваше имя: ")
 your_age = int(input("Введите Ваш возраст: "))
 course = "Python"
 print(f"Привет {your_name}, Вам {your_age} и вы обучаетесь на курсе {course}")
-print("Test")
