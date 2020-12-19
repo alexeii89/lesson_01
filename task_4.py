e99bcdffcb523cbc80f574a69819e4bab49abb52
ALexeii <mr.white1989@rambler.ru>
2020-12-20 01:24:57 +0500

    edit

--- task_4.py
+++ task_4.py
@@ -1,12 +1,9 @@
 natural_number = int(input("Введите целое число: "))
-
-i = 1
 max = natural_number % 10
-while natural_number == 0:
+while natural_number != 0:
     if max < natural_number % 10:
-        max = natural_namber % 10
+        max = natural_number % 10
         natural_number = natural_number // 10
     else:
         natural_number = natural_number // 10
-
-    print(f"Большее число: {max}")
+print(f"Большее число: {max}")
